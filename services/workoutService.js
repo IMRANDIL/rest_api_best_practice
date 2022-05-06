a6658fd88e32ee0f4f4647f6e98d84be55b6357b
IMRANDIL <aliimranadil2@gmail.com>
2022-05-06 23:18:27 +0530

    yh

---
+++ services/workoutService.js
@@ -0,0 +1,46 @@
+
+
+
+
+//create service for all the routes...
+
+
+//get all workouts...
+
+
+exports.getAllWorkouts = () => {
+
+}
+
+
+
+//get one workout....
+
+
+exports.getOneWorkout = () => {
+
+}
+
+
+
+//create new workout....
+
+
+exports.createNewWorkout = () => {
+
+}
+
+
+
+//update one workout...
+
+exports.updateOneWorkout = () => {
+
+}
+
+
+//delete one wokrout...
+
+exports.deleteOneWorkout = () => {
+
+}
