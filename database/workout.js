30ed67c7c9984a953cfda377ac9347380c93fd48
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 08:33:32 +0530

    yh

--- database/workout.js
+++ database/workout.js
@@ -1,10 +1,26 @@
 const DB = require('./db.json');
 
-
+const { saveToDB } = require('./utils')
 
 //create a method that returns all the workouts in db json...
 
 
 exports.getAllWorkouts = () => {
     return DB.workouts;
+}
+
+
+//now a method for creating new workouts...
+
+
+exports.createNewWorkout = (newWorkout) => {
+    //now check if it is already added...
+
+    const isAlreadyAdded = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+    if (isAlreadyAdded) {
+        throw new Error('This workout already Exists!')
+    }
+    DB.workouts.push(newWorkout);
+    saveToDB(DB);
+    return newWorkout;
 }
