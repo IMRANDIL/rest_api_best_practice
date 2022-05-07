eb1cac2b2f15dca3dd1f8a50e66ea0ca47f50752
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 08:26:30 +0530

    yh

---
+++ database/utils.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+
+//create a utility function that save the data to the filesystem..
+
+
+
+exports.saveToDB = (DB) => {
+    fs.writeFileSync(path.join(__dirname, 'database', 'db.json'), JSON.stringify(DB, null, 2), { encoding: 'utf-8' })
+}
