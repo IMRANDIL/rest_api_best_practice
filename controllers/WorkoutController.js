4b95911741491173d36cadbc7e3c28eddc8887e8
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 10:08:43 +0530

    yh

--- controllers/WorkoutController.js
+++ controllers/WorkoutController.js
@@ -127,14 +127,18 @@ exports.deleteOneWorkout = (req, res) => {
     const { workoutId } = req.params;
 
     if (!workoutId) {
-        return res.status(400).send('WorkoutId required')
+        return res.status(400).send({ status: "FAILED", data: { error: "Parameter ':workoutId' can not be empty" } })
     }
 
 
+    try {
+        const deletedWorkout = deleteOneWorkout(workoutId);
 
-    const deletedWorkout = deleteOneWorkout(workoutId);
+        //now send the response...
 
-    //now send the response...
+        res.status(204).send({ status: "OK" })
+    } catch (error) {
+        res.status(error?.status || 500).send({ status: "FAILED", data: { error: error?.message || error } })
+    }
 
-    res.status(200).send({ status: "OK" })
 }
